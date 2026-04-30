736388c85d001ec33ad6ee1a520c0e8b139640d1
Alan Basilio <2319754+alanbasilio@users.noreply.github.com>
2026-04-30 16:14:24 -0300

    chore: updating colors and configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Press_Start_2P } from "next/font/google";
+import { Geist, Press_Start_2P } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
 import { AudioProvider } from "@/providers/audio-provider";
 import { GameProvider } from "@/providers/game-provider";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 const ps2 = Press_Start_2P({ subsets: ["latin"], weight: ["400"] });
 
@@ -19,7 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("font-sans", geist.variable)}
+    >
       <body className={`${ps2.className} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="light">
           <AudioProvider>
